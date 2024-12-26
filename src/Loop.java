5da867d54b43d47c2bb23d6caf718e6b67cce63e
Marcelo Otsuki <marceloaires7@gmail.com>
2024-12-26 19:26:38 -0300

    Loop

--- src/Loop.java
+++ src/Loop.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Diga sua avaliação para o filme: ");
+            nota = sc.nextDouble();
+            mediaAvaliacao += nota;
+        }
+
+        System.out.println("Média de avaliações: " + mediaAvaliacao/3);
+
+        sc.close();
+    }
 }
